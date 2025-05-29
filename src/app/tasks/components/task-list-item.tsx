8b5208797634308ad5a953361ee5900c46c888df
Erik Bolumburu <108585396+erikbolumburu11@users.noreply.github.com>
2025-05-29 17:24:33 +0100

    Can update tasks in tasks view

--- src/app/tasks/components/task-list-item.tsx
+++ src/app/tasks/components/task-list-item.tsx
@@ -1,11 +1,24 @@
 import { Task } from "@/app/contexts/taskcontext";
+import axios from "axios";
 import { useState } from "react";
 
 export default function TaskListItem({task} : {task: Task}){
     const [done, setDone] = useState(task.done);
 
     const handleChecked = () => {
-        setDone(!done);
+        const isDone: boolean = !done;
+
+        setDone(isDone);
+
+        axios.post('http://localhost:8080/tasks/setdone/', {
+            taskid: task.id,
+            done: isDone 
+        }, {
+            withCredentials: true
+        })
+        .catch((error) => {
+            console.log(error);
+        })
     }
 
     return (
