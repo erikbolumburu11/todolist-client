43ca45442404d17890aae85f64aaee1d2b127e81
Erik Bolumburu <108585396+erikbolumburu11@users.noreply.github.com>
2025-06-04 11:37:29 +0100

    Whoops wrong env variable

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ export default nextConfig;
 const environment = process.env.NODE_ENV;
 let API_CONNECTION_STRING = "http://localhost:8080"
 if(environment === "production"){
-  API_CONNECTION_STRING = process.env.NEXT_PUBLIC_API_KEY!;
+  API_CONNECTION_STRING = process.env.NEXT_PUBLIC_API_URL!;
 }
 
 export { API_CONNECTION_STRING };
