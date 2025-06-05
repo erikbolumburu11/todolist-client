c3239430e74fa8ce546586a672fd4e913c9564a1
Erik Bolumburu <108585396+erikbolumburu11@users.noreply.github.com>
2025-06-05 21:27:41 +0100

    Removed vertical padding on tasks title

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -2,7 +2,7 @@ import TaskList from "./components/tasks/task-list";
 
 export default function Tasks() {
     return(
-        <div className="m-10">
+        <div className="mx-10">
             <h1 className="md:text-start text-center">Tasks</h1>
             <div className="mt-3">
                 <TaskList/>
